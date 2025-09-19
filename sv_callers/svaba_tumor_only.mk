bea6d6e0cbd1aa857bade005169daba677b05735
David Brown <ndbrown6@gmail.com>
2025-09-19 09:56:06 -0400

    Update svaba_tumor_only.mk

--- sv_callers/svaba_tumor_only.mk
+++ sv_callers/svaba_tumor_only.mk
@@ -9,10 +9,11 @@ SVABA_DBSNP ?= $(HOME)/share/lib/resource_files/svaba/dbsnp_indel.vcf
 SVABA_BLACKLIST ?= $(HOME)/share/lib/resource_files/svaba/wgs_blacklist_meres.bed
 SVABA ?= svaba
 
-svaba : $(foreach sample,$(SAMPLES),vcf/$(sample).svaba_sv.vcf)
+svaba : $(foreach sample,$(SAMPLES),vcf/$(sample).svaba_sv.vcf) \
+	$(foreach sample,$(SAMPLES),vcf/$(sample).svaba_indels.vcf)
 
 define svaba-tumor-only
-svaba/$1.svaba.somatic.sv.vcf : bam/$1.bam
+svaba/$1.svaba.sv.vcf svaba/$1.svaba.indel.vcf : bam/$1.bam
 	$$(call RUN,-c -n $(SVABA_CORES) -s 4G -m $(SVABA_MEM_CORE) -v $(SVABA_ENV) -w 72:00:00,"set -o pipefail && \
 												 mkdir -p svaba && \
 										 		 cd svaba && \
@@ -25,7 +26,10 @@ svaba/$1.svaba.somatic.sv.vcf : bam/$1.bam
 												 -a $1 \
 												 -G $$(SVABA_REF)")
 												 
-vcf/$1.svaba_sv.vcf : svaba/$1.svaba.somatic.sv.vcf
+vcf/$1.svaba_sv.vcf : svaba/$1.svaba.sv.vcf
+	$$(INIT) cat $$< > $$@
+	
+vcf/$1.svaba_indels.vcf : svaba/$1.svaba.indel.vcf
 	$$(INIT) cat $$< > $$@
 
 endef
