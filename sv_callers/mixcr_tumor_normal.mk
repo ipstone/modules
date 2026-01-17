2b0b6d1b5b66280bcc5c009a8828c993f8eb51b4
David Brown <ndbrown6@gmail.com>
2026-01-16 19:07:26 -0500

    Update mixcr_tumor_normal.mk

--- sv_callers/mixcr_tumor_normal.mk
+++ sv_callers/mixcr_tumor_normal.mk
@@ -7,7 +7,8 @@ mixcr : $(foreach sample,$(SAMPLES),mixcr/$(sample)/$(sample).1.fastq.gz) \
 	$(foreach sample,$(SAMPLES),mixcr/$(sample)/alignments_rescued_1.vdjca) \
 	$(foreach sample,$(SAMPLES),mixcr/$(sample)/alignments_rescued_2.vdjca) \
 	$(foreach sample,$(SAMPLES),mixcr/$(sample)/alignments_rescued_2_extended.vdjca) \
-	$(foreach sample,$(SAMPLES),mixcr/$(sample)/clones.clns) 
+	$(foreach sample,$(SAMPLES),mixcr/$(sample)/clones.clns) \
+	$(foreach sample,$(SAMPLES),mixcr/$(sample)/clones.tsv)
 
 define extract-fastq
 mixcr/$1/$1.1.fastq : bam/$1.bam
@@ -60,6 +61,12 @@ mixcr/$1/clones.clns : mixcr/$1/alignments_rescued_2_extended.vdjca
 	$$(call RUN,-n 8 -s 4G -m 6G -v $(MIXCR_ENV) -w 24:00:00,"set -o pipefail && \
 								  mixcr assemble \
 								  -O badQualityThreshold=0 \
+								  $$(<) \
+								  $$(@)")
+								  
+mixcr/$1/clones.tsv : mixcr/$1/clones.clns
+	$$(call RUN,-n 8 -s 4G -m 6G -v $(MIXCR_ENV) -w 24:00:00,"set -o pipefail && \
+								  mixcr exportClones \
 								  $$(<) \
 								  $$(@)")
 								  
