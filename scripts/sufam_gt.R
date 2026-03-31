81fde5d21cf9a50ccc140e430fa385bf16c901f5
ndbrown6 <ndbrown6@gmail.com>
2026-03-31 15:13:48 -0400

    ++

--- scripts/sufam_gt.R
+++ scripts/sufam_gt.R
@@ -12,10 +12,10 @@ if (!interactive()) {
 
 optList = list(make_option("--option", default = NA, type = 'character', help = "analysis type"),
                make_option("--sample_set", default = NA, type = 'character', help = "sample set"),
-	       make_option("--tumor_sample", default = NA, type = 'character', help = "tumor sample"),
-	       make_option("--normal_sample", default = NA, type = 'character', help = "normal sample"),
-	       make_option("--input_file", default = NA, type = 'character', help = "input file"),
-	       make_option("--output_file", default = NA, type = 'character', help = "output file"))
+		       make_option("--tumor_sample", default = NA, type = 'character', help = "tumor sample"),
+		       make_option("--normal_sample", default = NA, type = 'character', help = "normal sample"),
+		       make_option("--input_file", default = NA, type = 'character', help = "input file"),
+		       make_option("--output_file", default = NA, type = 'character', help = "output file"))
 parser = OptionParser(usage = "%prog", option_list = optList)
 arguments = parse_args(parser, positional_arguments = T)
 opt = arguments$options
@@ -52,58 +52,103 @@ if (as.numeric(opt$option)==1) {
 	readr::write_tsv(x = smry, path = as.character(opt$output_file), append = TRUE, col_names = TRUE)
 
 } else if (as.numeric(opt$option)==2) {
-	tumor_sample = unlist(strsplit(x = as.character(opt$tumor_sample), split = " ", fixed=TRUE))
-	normal_sample = unlist(strsplit(x = as.character(opt$normal_sample), split = " ", fixed=TRUE))
-	maf = readr::read_tsv(file = opt$input_file, comment = "#", col_names = TRUE, col_types = cols(.default = col_character())) %>%
-	      readr::type_convert() %>%
-	      dplyr::mutate(chrom = Chromosome,
-			    loc.start = Start_Position,
-			    loc.end = End_Position) %>%
-	      dplyr::mutate(chrom = as.character(chrom))
-	facets = readr::read_tsv(file = paste0("facets/cncf/", tumor_sample, "_", normal_sample, ".txt"), col_names = TRUE, col_types = cols(.default = col_character())) %>%
-		 dplyr::mutate(chrom = case_when(
-			 chrom == "23" ~ "X",
-			 TRUE ~ chrom
-		 )) %>%
-		 readr::type_convert() %>%
-		 dplyr::mutate(qt = tcn.em,
-			       q2 = tcn.em - lcn.em) %>%
-		 dplyr::select(chrom, loc.start, loc.end, qt, q2)
-	maf = maf %>%
-	      fuzzyjoin::genome_left_join(facets, by = c("chrom", "loc.start", "loc.end")) %>%
-	      dplyr::select(-chrom.x, -loc.start.x, -loc.end.x, -chrom.y, -loc.start.y, -loc.end.y)
-			
-	write_tsv(x = maf, path = as.character(opt$output_file), append = FALSE, col_names = TRUE)
-
-} else if (as.numeric(opt$option)==3) {
 	sample_set = unlist(strsplit(x = as.character(opt$sample_set), split = " ", fixed=TRUE))
 	normal_sample = unlist(strsplit(x = as.character(opt$normal_sample), split = " ", fixed=TRUE))
 	sample_set = setdiff(sample_set, normal_sample)
-	maf = list()
-	for (i in 1:length(sample_set)) {
-		sufam = readr::read_tsv(file = paste0("sufam/", sample_set[i], ".txt"), col_names = TRUE, col_types = cols(.default = col_character())) %>%
+	smry = readr::read_tsv(file = as.character(opt$input_file), col_names = TRUE, col_types = cols(.default = col_character())) %>%
+	       readr::type_convert() %>%
+	       dplyr::filter(TUMOR_SAMPLE %in% sample_set) %>%
+	       dplyr::filter(NORMAL_SAMPLE == normal_sample) %>%
+	       dplyr::mutate(UUID = paste0(CHROM, ":", POS, "_", REF, ">", ALT)) %>%
+	       dplyr::filter(!duplicated(UUID)) %>%
+	       dplyr::mutate(`#CHROM` = CHROM,
+			     POS = POS,
+			     ID = ".",
+			     REF = REF,
+			     ALT = ALT,
+			     QUAL = 100,
+			     FILTER = "PASS",
+			     INFO = ".") %>%
+	       dplyr::select(`#CHROM`, POS, ID, REF, ALT, QUAL, INFO) %>%
+	       dplyr::mutate(`#CHROM` = as.character(`#CHROM`)) %>%
+	       dplyr::mutate(chr_n = case_when(
+		       `#CHROM` == "X" ~ "23",
+		       `#CHROM` == "Y" ~ "24",
+		       TRUE ~ `#CHROM`
+	       )) %>%
+	       readr::type_convert() %>%
+	       dplyr::arrange(chr_n) %>%
+	       dplyr::select(-chr_n)
+	cat("##fileformat=VCFv4.2\n", file = as.character(opt$output_file), append=FALSE)
+	readr::write_tsv(x = smry, path = as.character(opt$output_file), append = TRUE, col_names = TRUE)
+
+} else if (as.numeric(opt$option)==3) {
+	tumor_sample = unlist(strsplit(x = as.character(opt$tumor_sample), split = " ", fixed=TRUE))
+	normal_sample = unlist(strsplit(x = as.character(opt$normal_sample), split = " ", fixed=TRUE))
+	
+	t_maf = readr::read_tsv(file = paste0("sufam/", tumor_sample, ".maf"), comment = "#", col_names = TRUE, col_types = cols(.default = col_character())) %>%
+		    readr::type_convert()
+	t_gt = readr::read_tsv(file = paste0("sufam/", tumor_sample, ".txt"), col_names = TRUE, col_types = cols(.default = col_character())) %>%
+		   readr::type_convert() %>%
+		   dplyr::select(CHROM = chrom,
+					     POS = pos,
+					     REF = val_ref,
+					     ALT = val_alt,
+					     t_depth = cov,
+					     t_alt_count = val_al_count)
+	t_cn = readr::read_tsv(file = paste0("facets/cncf/", tumor_sample, "_", normal_sample, ".txt"), col_names = TRUE, col_types = cols(.default = col_character())) %>%
+		   dplyr::mutate(chrom = case_when(
+							 chrom == "23" ~ "X",
+							 TRUE ~ chrom
+			)) %>%
 			readr::type_convert() %>%
-			dplyr::select(CHROM = chrom,
-				      POS = pos,
-				      REF = val_ref,
-				      ALT = val_alt,
-				      t_depth = cov,
-				      t_alt_count = val_al_count) %>%
-		 	dplyr::mutate(t_ref_count = t_depth - t_alt_count)
+			dplyr::mutate(qt = tcn.em,
+					      q2 = tcn.em - lcn.em) %>%
+			dplyr::select(Chromosome = chrom,
+						  Start_Position = loc.start,
+						  End_Position = loc.end,
+						  qt, q2)
+
+	n_maf = readr::read_tsv(file = paste0("sufam/", normal_sample, ".maf"), comment = "#", col_names = TRUE, col_types = cols(.default = col_character())) %>%
+		    readr::type_convert()
+	n_gt = readr::read_tsv(file = paste0("sufam/", normal_sample, ".txt"), col_names = TRUE, col_types = cols(.default = col_character())) %>%
+		   readr::type_convert() %>%
+		   dplyr::select(CHROM = chrom,
+					     POS = pos,
+					     REF = val_ref,
+					     ALT = val_alt,
+					     n_depth = cov,
+					     n_alt_count = val_al_count)
+					     
+	t_maf = t_maf %>%
+			dplyr::select(-t_depth, -t_ref_count, -t_alt_count, -n_depth, -n_ref_count, -n_alt_count) %>%
+			dplyr::bind_cols(t_gt) %>%
+		    dplyr::mutate(t_ref_count = t_depth - t_alt_count) %>%
+		    dplyr::mutate(Chromosome = as.character(Chromosome)) %>%
+		    dplyr::left_join(n_maf %>%
+		    				 dplyr::select(-t_depth, -t_ref_count, -t_alt_count, -n_depth, -n_ref_count, -n_alt_count) %>%
+		  					 dplyr::bind_cols(n_gt) %>%
+		  					 dplyr::select(Chromosome, Start_Position, End_Position, Reference_Allele, Tumor_Seq_Allele2,
+		  					 			   n_depth, n_alt_count) %>%
+		  					 dplyr::mutate(Chromosome = as.character(Chromosome)),
+		  					 by = c("Chromosome", "Start_Position", "End_Position", "Reference_Allele", "Tumor_Seq_Allele2")) %>%
+		  	dplyr::mutate(n_ref_count = n_depth - n_alt_count) %>%
+		  	dplyr::mutate(Tumor_Sample_Barcode = tumor_sample,
+		  				  Matched_Norm_Sample_Barcode = normal_sample) %>%
+		  	dplyr::mutate(Chromosome = as.character(Chromosome)) %>%
+	        fuzzyjoin::genome_left_join(t_cn, by = c("Chromosome", "Start_Position", "End_Position")) %>%
+	        dplyr::select(-Chromosome.y, -Start_Position.y, -End_Position.y) %>%
+	        dplyr::rename(Chromosome = Chromosome.x,
+	    				  Start_Position = Start_Position.x,
+	    				  End_Position = End_Position.x)
 			
-		maf[[i]] = readr::read_tsv(file = paste0("sufam/", sample_set[i], "_ann.maf"), comment = "#", col_names = TRUE, col_types = cols(.default = col_character())) %>%
-		      	   readr::type_convert() %>%
-		      	   dplyr::select(-t_depth, -t_alt_count, -t_ref_count) %>%
-		      	   dplyr::bind_cols(sufam)
-	}
-	maf = do.call(bind_rows, maf)
-	write_tsv(x = maf, path = as.character(opt$output_file), append = FALSE, col_names = TRUE)
+	write_tsv(x = t_maf, path = as.character(opt$output_file), append = FALSE, col_names = TRUE)
 
 } else if (as.numeric(opt$option)==4) {
-	sample_set = unlist(strsplit(x = as.character(opt$sample_set), split = " ", fixed=TRUE))
+	sample_pairs = unlist(strsplit(x = as.character(opt$sample_set), split = " ", fixed=TRUE))
 	maf = list()
-	for (i in 1:length(sample_set)) {
-		maf[[i]] = readr::read_tsv(file = paste0("sufam/", sample_set[i], ".maf"), comment = "#", col_names = TRUE, col_types = cols(.default = col_character()))
+	for (i in 1:length(sample_pairs)) {
+		maf[[i]] = readr::read_tsv(file = paste0("sufam/", sample_pairs[i], ".ann.maf"), comment = "#", col_names = TRUE, col_types = cols(.default = col_character()))
 	}
 	maf = do.call(bind_rows, maf) %>%
 	      readr::type_convert()
@@ -141,18 +186,18 @@ if (as.numeric(opt$option)==1) {
 	       readr::type_convert()
 	maf = maf %>%
 	      dplyr::left_join(smry %>%
-			       dplyr::group_by(CHROM, POS, REF, ALT) %>%
-	       		       dplyr::summarize(is_Hotspot = unique(is_Hotspot)) %>%
-			       dplyr::ungroup(),
-			       by = c("CHROM", "POS", "REF", "ALT"))
+					       dplyr::group_by(CHROM, POS, REF, ALT) %>%
+		       		       dplyr::summarize(is_Hotspot = unique(is_Hotspot)) %>%
+					       dplyr::ungroup(),
+					       by = c("CHROM", "POS", "REF", "ALT"))
 	maf = maf %>%
 	      dplyr::left_join(smry %>%
-			       dplyr::select(CHROM, POS, REF, ALT, Tumor_Sample_Barcode = TUMOR_SAMPLE, is_LOH) %>%
-			       dplyr::mutate(is_present = TRUE),
-			       by = c("CHROM", "POS", "REF", "ALT", "Tumor_Sample_Barcode")) %>%
+					       dplyr::select(CHROM, POS, REF, ALT, Tumor_Sample_Barcode = TUMOR_SAMPLE, is_LOH) %>%
+					       dplyr::mutate(is_present = TRUE),
+					       by = c("CHROM", "POS", "REF", "ALT", "Tumor_Sample_Barcode")) %>%
 	      dplyr::mutate(is_present = case_when(
-		      is.na(is_present) ~ FALSE,
-		      TRUE ~ is_present
+						      is.na(is_present) ~ FALSE,
+						      TRUE ~ is_present
 	      ))
 	write_tsv(x = maf, path = as.character(opt$output_file), append = FALSE, col_names = TRUE)
 
@@ -162,4 +207,3 @@ if (as.numeric(opt$option)==1) {
 	      dplyr::filter(is_present)
 	write_tsv(x = maf, path = as.character(opt$output_file), append = FALSE, col_names = TRUE)
 }
-
--- variant_callers/sufam_gt.mk
+++ variant_callers/sufam_gt.mk
@@ -6,87 +6,117 @@ SUFAM_ENV = $(HOME)/share/usr/anaconda-envs/sufam-dev
 SUFAM_OPTS = --mpileup-parameters='-A -q 15 -Q 15 -d 15000'
 
 sufam_gt : $(foreach sample,$(TUMOR_SAMPLES),sufam/$(sample).vcf) \
-	   $(foreach sample,$(TUMOR_SAMPLES),sufam/$(sample).txt) \
-	   $(foreach sample,$(TUMOR_SAMPLES),sufam/$(sample).maf) \
-	   $(foreach sample,$(TUMOR_SAMPLES),sufam/$(sample)_ann.maf) \
-	   $(foreach set,$(SAMPLE_SETS),sufam/$(set).maf) \
-	   sufam/mutation_summary.maf \
-	   sufam/mutation_summary_ft.maf
+		   $(foreach sample,$(TUMOR_SAMPLES),sufam/$(sample).txt) \
+		   $(foreach sample,$(TUMOR_SAMPLES),sufam/$(sample).maf) \
+		   $(foreach sample,$(NORMAL_SAMPLES),sufam/$(sample).vcf) \
+		   $(foreach sample,$(NORMAL_SAMPLES),sufam/$(sample).txt) \
+		   $(foreach sample,$(NORMAL_SAMPLES),sufam/$(sample).maf) \
+		   $(foreach pair,$(SAMPLE_PAIRS),sufam/$(pair).ann.maf) \
+		   sufam/mutation_summary.maf \
+		   sufam/mutation_summary_ft.maf
 
-define sufam-gt
+define sufam-gt-tumor
 sufam/$1.vcf : summary/tsv/all.tsv
 	$$(call RUN,-c -n 1 -s 4G -m 8G -v $(INNOVATION_ENV),"set -o pipefail && \
-							      $(RSCRIPT) $(SCRIPTS_DIR)/sufam_gt.R \
-							      --option 1 \
-							      --sample_set '$(set.$1)' \
-							      --normal_sample '$(normal.$1)' \
-							      --input_file $$(<) \
-							      --output_file $$(@)")
+													      $(RSCRIPT) $(SCRIPTS_DIR)/sufam_gt.R \
+													      --option 1 \
+													      --sample_set '$(set.$1)' \
+													      --normal_sample '$(normal.$1)' \
+													      --input_file $$(<) \
+													      --output_file $$(@)")
 					 
 sufam/$1.txt : sufam/$1.vcf bam/$1.bam
 	$$(call RUN,-c -n 1 -s 2G -m 3G -v $(SUFAM_ENV),"set -o pipefail && \
-					 		 sufam \
-							 --sample_name $1 \
-							 $$(SUFAM_OPTS) \
-							 $$(REF_FASTA) \
-							 $$(<) \
-							 $$(<<) \
-							 > $$(@)")
+											 		 sufam \
+													 --sample_name $1 \
+													 $$(SUFAM_OPTS) \
+													 $$(REF_FASTA) \
+													 $$(<) \
+													 $$(<<) \
+													 > $$(@)")
 
 sufam/$1.maf : sufam/$1.vcf
 	$$(call RUN,-c -n 12 -s 1G -m 2G -v $(VEP_ENV),"set -o pipefail && \
-							$$(VCF2MAF) \
-							--input-vcf $$< \
-							--tumor-id $1 \
-							--filter-vcf $$(EXAC_NONTCGA) \
-							--ref-fasta $$(REF_FASTA) \
-							--vep-path $$(VEP_PATH) \
-							--vep-data $$(VEP_DATA) \
-							--tmp-dir `mktemp -d` \
-							--output-maf $$(@)")
-							
-sufam/$1_ann.maf : sufam/$1.maf
-	$$(call RUN,-c -n 1 -s 2G -m 3G -v $(INNOVATION_ENV),"set -o pipefail && \
-							      $(RSCRIPT) $(SCRIPTS_DIR)/sufam_gt.R \
-							      --option 2 \
-							      --tumor_sample $1 \
-							      --normal_sample '$(normal.$1)' \
-							      --input_file $$(<) \
-							      --output_file $$(@)")
-
+													$$(VCF2MAF) \
+													--input-vcf $$< \
+													--tumor-id $1 \
+													--normal-id NA \
+													--filter-vcf $$(EXAC_NONTCGA) \
+													--ref-fasta $$(REF_FASTA) \
+													--vep-path $$(VEP_PATH) \
+													--vep-data $$(VEP_DATA) \
+													--tmp-dir `mktemp -d` \
+													--output-maf $$(@)")
 
 endef
 $(foreach sample,$(TUMOR_SAMPLES),\
-		$(eval $(call sufam-gt,$(sample))))
+		$(eval $(call sufam-gt-tumor,$(sample))))
 		
-define combine-maf
-sufam/$1.maf : $(foreach sample,$(TUMOR_SAMPLES),sufam/$(sample).txt) $(foreach sample,$(TUMOR_SAMPLES),sufam/$(sample)_ann.maf)
+define sufam-gt-normal
+sufam/$1.vcf : summary/tsv/all.tsv
 	$$(call RUN,-c -n 1 -s 4G -m 8G -v $(INNOVATION_ENV),"set -o pipefail && \
-							      $(RSCRIPT) $(SCRIPTS_DIR)/sufam_gt.R \
-							      --option 3 \
-							      --sample_set '$(set.$1)' \
-							      --normal_sample '$(normal.$1)' \
-							      --output_file $$(@)")
+													      $(RSCRIPT) $(SCRIPTS_DIR)/sufam_gt.R \
+													      --option 2 \
+													      --sample_set '$(set.$1)' \
+													      --normal_sample $1 \
+													      --input_file $$(<) \
+													      --output_file $$(@)")
 					 
-endef
-$(foreach set,$(SAMPLE_SETS),\
-		$(eval $(call combine-maf,$(set))))
-		
+sufam/$1.txt : sufam/$1.vcf bam/$1.bam
+	$$(call RUN,-c -n 1 -s 2G -m 3G -v $(SUFAM_ENV),"set -o pipefail && \
+											 		 sufam \
+													 --sample_name $1 \
+													 $$(SUFAM_OPTS) \
+													 $$(REF_FASTA) \
+													 $$(<) \
+													 $$(<<) \
+													 > $$(@)")
 
-sufam/mutation_summary.maf : summary/tsv/all.tsv $(foreach set,$(SAMPLE_SETS),sufam/$(set).maf)
+sufam/$1.maf : sufam/$1.vcf
+	$$(call RUN,-c -n 12 -s 1G -m 2G -v $(VEP_ENV),"set -o pipefail && \
+													$$(VCF2MAF) \
+													--input-vcf $$< \
+													--tumor-id NA \
+													--normal-id $1 \
+													--filter-vcf $$(EXAC_NONTCGA) \
+													--ref-fasta $$(REF_FASTA) \
+													--vep-path $$(VEP_PATH) \
+													--vep-data $$(VEP_DATA) \
+													--tmp-dir `mktemp -d` \
+													--output-maf $$(@)")
+
+endef
+$(foreach sample,$(NORMAL_SAMPLES),\
+		$(eval $(call sufam-gt-normal,$(sample))))
+
+define sufam-gt-pair
+sufam/$1_$2.ann.maf : sufam/$1.maf sufam/$2.maf sufam/$1.txt sufam/$2.txt facets/cncf/$1_$2.txt
+	$$(call RUN,-c -n 1 -s 2G -m 3G -v $(INNOVATION_ENV),"set -o pipefail && \
+													      $(RSCRIPT) $(SCRIPTS_DIR)/sufam_gt.R \
+													      --option 3 \
+													      --tumor_sample $1 \
+													      --normal_sample $2 \
+													      --output_file $$(@)")
+													      
+endef
+$(foreach pair,$(SAMPLE_PAIRS),\
+	$(eval $(call sufam-gt-pair,$(tumor.$(pair)),$(normal.$(pair)))))
+
+
+sufam/mutation_summary.maf : summary/tsv/all.tsv $(foreach pair,$(SAMPLE_PAIRS),sufam/$(pair).ann.maf)
 	$(call RUN, -c -n 1 -s 8G -m 12G -v $(INNOVATION_ENV),"set -o pipefail && \
-							       $(RSCRIPT) $(SCRIPTS_DIR)/sufam_gt.R \
-							       --option 4 \
-							       --sample_set '$(SAMPLE_SETS)' \
-							       --input_file $(<) \
-							       --output_file $(@)")
+													       $(RSCRIPT) $(SCRIPTS_DIR)/sufam_gt.R \
+													       --option 4 \
+													       --sample_set '$(SAMPLE_PAIRS)' \
+													       --input_file $(<) \
+													       --output_file $(@)")
 							       
 sufam/mutation_summary_ft.maf : sufam/mutation_summary.maf
 	$(call RUN, -c -n 1 -s 8G -m 12G -v $(INNOVATION_ENV),"set -o pipefail && \
-							       $(RSCRIPT) $(SCRIPTS_DIR)/sufam_gt.R \
-							       --option 5 \
-							       --input_file $(<) \
-							       --output_file $(@)")
+													       $(RSCRIPT) $(SCRIPTS_DIR)/sufam_gt.R \
+													       --option 5 \
+													       --input_file $(<) \
+													       --output_file $(@)")
 
 ..DUMMY := $(shell mkdir -p version; \
 	     R --version > version/sufam_gt.txt)
